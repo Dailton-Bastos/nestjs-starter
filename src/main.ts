6e52e2f1f4bff124e53faa67c56fcca5e46cf9f4
Dailton Bastos <dailtonbastos@gmail.com>
2025-11-26 21:07:30 -0300

    feat(main): add dev as default node env

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
 	const PORT = process.env.PORT ?? 3000
+	const ENV = process.env.NODE_ENV ?? 'development'
+
 	await app.listen(PORT, () => {
 		const logger = new Logger('Bootstrap')
-		logger.log(
-			`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`
-		)
+		logger.log(`Server is running on port ${PORT} in ${ENV} mode`)
 	})
 }
 bootstrap()
