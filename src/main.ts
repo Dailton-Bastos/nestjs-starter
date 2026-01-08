a605230bfb1b066b3ae556f15a38f7e44270a42c
Dailton Bastos <dailtonbastos@gmail.com>
2026-01-08 18:53:24 -0300

    feat(app): add default config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { appConfig } from './config/app.config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
-	const PORT = process.env.PORT ?? 3000
-	const ENV = process.env.NODE_ENV ?? 'development'
+	appConfig(app)
+
+	const configService = app.get(ConfigService)
+
+	const PORT = configService.getOrThrow<number>('PORT')
+	const ENV = configService.getOrThrow<string>('NODE_ENV')
 
 	await app.listen(PORT, () => {
 		const logger = new Logger('Bootstrap')
