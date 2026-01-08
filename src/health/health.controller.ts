fda8c123fcc808ac589bc2bb9df9be7df604c047
Dailton Bastos <dailtonbastos@gmail.com>
2026-01-08 17:17:37 -0300

    chore(biome): add warn for console and disable useImportType

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get } from '@nestjs/common'
-// biome-ignore lint/style/useImportType: Nest can't resolve dependencies
 import { HealthService } from './health.service'
 
 @Controller('health')
